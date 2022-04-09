95728275a9c81c15376c5760cc58d624a7f280c6
Fahad Hossain <64582073+Fahadhossain99@users.noreply.github.com>
2022-04-09 10:01:47 +0600

    multiple screens

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from './src/screens/home';
+import Login from './src/screens/login';
+import Create from './src/screens/create';
+import Update from './src/screens/update';
+import Signup from './src/screens/signup';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const [user,setuser]=React.useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {user ? (
+          <>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Create" component={Create} />
+            <Stack.Screen name="Update" component={Update} />
+          </>
+        ) : (
+          <><Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} /></>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+
